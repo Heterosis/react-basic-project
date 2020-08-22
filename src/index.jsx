26f5d76e1eabb8e9a7ffb30b00f6d8ddf598631a
Heterosis <a3817001@gmail.com>
2020-08-22 10:30:42 +0800

    Basic react project

---
+++ src/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/main.css';
+
+class App extends React.Component {
+  static sayHi() {
+    return 'Hi';
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { name } = this.props;
+    return (
+      <div>
+        <h1>Hello, World!!</h1>
+        <h2>{this.constructor.sayHi()}</h2>
+        <h3>{name}</h3>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App name="neil" />, document.getElementById('app'));
---
+++ webpack.common.js
@@ -0,0 +1,72 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  target: 'web',
+  entry: ['@babel/polyfill', path.resolve('src', 'index.jsx')],
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'eslint-loader',
+            options: {
+              fix: true,
+            },
+          },
+          {
+            loader: 'prettier-loader',
+            options: {
+              parser: 'babel',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: './',
+            },
+          },
+          { loader: 'css-loader' },
+        ],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|svg|jpe?g)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[folder]/[name].[ext]',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.html'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: '[name].css?h=[hash]' }),
+    new HtmlWebpackPlugin({
+      template: path.resolve('src', 'index.html'),
+      favicon: path.resolve('src', 'assets', 'favicon.ico'),
+    }),
+  ],
+  output: {
+    filename: '[name].js?h=[hash]',
+    path: path.resolve('dist'),
+  },
+};
