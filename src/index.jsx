adc99ac44cefc0ba97486a6cf5d6ccbe63251b87
Neil Jhang <a3817001@gmail.com>
2020-09-12 16:47:11 +0800

    Split modules

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/main.css';
-
-class App extends React.Component {
-  static sayHi() {
-    return 'Hi';
-  }
-
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const { name } = this.props;
-    return (
-      <div>
-        <h1>Hello, World!!</h1>
-        <h2>{this.constructor.sayHi()}</h2>
-        <h3>{name}</h3>
-      </div>
-    );
-  }
-}
+import './index.css';
+import App from './App';
 
 ReactDOM.render(<App name="react" />, document.getElementById('root'));
