0661ca94ee034e7664f35e775291bba3af1e1403
Heterosis <39578132+Heterosis@users.noreply.github.com>
2021-08-27 11:10:04 +0800

    Add Github link

--- src/compoents/App.jsx
+++ src/compoents/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import style from './App.module.css';
 
 const App = (props) => {
@@ -11,6 +12,13 @@ const App = (props) => {
       <h1 className={`${style.H1}`}>Hello, World!!</h1>
       <h2>{sayHi()}</h2>
       <h3>{name}</h3>
+      <footer className="position-fixed bottom-0 w-100">
+        <div className={`${style.ReverseRow}`}>
+          <a className={`${style.Link}`} href="https://github.com/Heterosis/react-basic-project" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={['fab', 'github']} style={{ fontSize: 40 }} />
+          </a>
+        </div>
+      </footer>
     </div>
   );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'css/index.css';
 import App from 'compoents/App';
+
+library.add(fab, fas, far);
 
 ReactDOM.render(<App name="react" />, document.getElementById('app'));
